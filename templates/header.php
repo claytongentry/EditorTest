f83f97aa4b08f151f41ea3408b82db371007faed
cg3ntry <cg3ntry@gmail.com>
2015-07-12 18:58:38 -0400

    Styles linked correctly

--- templates/header.php
+++ templates/header.php
@@ -4,8 +4,8 @@
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <link type = "text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <link href='http://fonts.googleapis.com/css?family=Oswald:700' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" type = "text/css" href = "./css/joyride-2.1.css"/>
-  <link href = "./css/main.css" type = "text/css" rel = "stylesheet"/>
+  <link rel="stylesheet" type = "text/css" href = "../css/joyride-2.1.css"/>
+  <link href = "../css/main.css" type = "text/css" rel = "stylesheet"/>
   <title>Edit Test</title>
 </head>
 <body>
